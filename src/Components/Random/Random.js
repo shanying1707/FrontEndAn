993f799ee0ce40ff45f03a40242292e50c973309
shanying1707 <shanying1707@gmail.com>
2023-10-25 20:58:11 +0700

    Buoi16

--- src/Components/Random/Random.js
+++ src/Components/Random/Random.js
@@ -1,33 +1,46 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Random.css'
 
 function Random() {
   const [max,setMax] = useState('')
-  const [min,setMin] = useState('')
+  const [disable,setDisable] = useState(false)
+  const [min,setMin] = useState({value: ''})
   const [random,setRandom] = useState(0)
   
   const handleMax = (e) => {
     setMax(Number(e.target.value))
   }
   const handleMin = (e) => {
-    setMin(Number(e.target.value))
-    if (min > max){
-      alert('no')
-    }
-    else{}
+
+    setMin(prev => ({...prev, value: Number(e.target.value)}) )
+      // Number(e.target.value))
+    console.log('min',min.value)
+    console.log('max',max)
+    
   }
   const handleRandom = () => {    
-      setRandom(Math.floor(Math.random() * (max - min) + min));   
+      
+    setRandom(Math.floor(Math.random() * (max - min.value) + min.value));   
   }
-    console.log(Math.floor((Math.random() * 10) + 1) +1)
+    const handleCheck = () =>{
+      if (min.value > max && max!==''){
+        alert('no')
+        setDisable(true)
+      }
+     
+     
+    }
+    useEffect(()=>{
+      handleCheck()
+    }, [min, max])
   return (
     <div className='container'>
         <div>{random}</div>
         <form>
             Max:<input type='number' placeholder='Type max number here' value={max} onChange={handleMax}/>
-            Min:<input type='number' placeholder='Type min number here' value={min} onChange={handleMin}/>
+            Min:<input type='number' placeholder='Type min number here' value={min.value} onChange={handleMin}/>
         </form>
-        <button onClick={handleRandom}>Generate</button>
+        <button onClick={handleRandom} disabled={disable}>Generate</button>
     </div>
   )
 }
